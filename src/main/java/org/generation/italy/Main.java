46412d1b801643818b08520d95ef5262c17f0faf
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-11-30 17:57:19 +0100

    change query

--- src/main/java/org/generation/italy/Main.java
+++ src/main/java/org/generation/italy/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
@@ -11,20 +12,36 @@ public class Main {
 		final String URL = "jdbc:mysql://localhost:8889/db-nations";
 		final String USER = "root";
 		final String PASSWORD = "root";
+		//apro lo scanner per chiedere un continenete all'user
+		Scanner sc = new Scanner(System.in);
+		System.out.println("carca un cintinente per nome; inserisci il nome del continenete");
+		String inputName = sc.nextLine();
+		//chiudo lo scanner
+		sc.close();
 		
 		try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
 			
 		
-			final String sql = "SELECT * FROM continents";
+			final String sql = "SELECT countries.country_id, continents.name, regions.name, countries.name"
+								+ "FROM continents "
+								+ "		JOIN regions\n"
+								+ "			ON regions.continent_id = continents.continent_id\n"
+								+ "		JOIN countries\n"
+								+ "			ON countries.country_id = regions.region_id"
+					   			+ "WHERE countries.name LIKE ?";
 					
 			try (PreparedStatement ps = connection.prepareStatement (sql)) {
+				ps.setString(1, inputName);
+				
 				try (ResultSet res = ps.executeQuery ( )){
 					while (res.next()) {
 						
 						int idContinent = res.getInt(1);
 						String continenetName = res.getString(2);
+						String regionsName = res.getString(3);
+						String countriesName = res.getString(4);
 						
-						System. out .println ( idContinent + "-" + continenetName);
+						System. out .println ( idContinent + "-" + continenetName + regionsName + "-" + countriesName );
 					}
 				}
 			}
