f1de88b48821908646200ad2abca1522cc1af5bf
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-11-30 17:28:29 +0100

    print sql query

--- src/main/java/org/generation/italy/Main.java
+++ src/main/java/org/generation/italy/Main.java
@@ -1,7 +1,36 @@
 package org.generation.italy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello poyo");
+		
+		final String URL = "jdbc:mysql://localhost:8889/db-nations";
+		final String USER = "root";
+		final String PASSWORD = "root";
+		
+		try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+			
+		
+			final String sql = "SELECT * FROM continents";
+					
+			try (PreparedStatement ps = connection.prepareStatement (sql)) {
+				try (ResultSet res = ps.executeQuery ( )){
+					while (res.next()) {
+						
+						int idContinent = res.getInt(1);
+						String continenetName = res.getString(2);
+						
+						System. out .println ( idContinent + "-" + continenetName);
+					}
+				}
+			}
+				
+		} catch (Exception e) {
+			System.err.println("ERROR: ' + e .getMessage ()") ;
+		}
 	}
 }
